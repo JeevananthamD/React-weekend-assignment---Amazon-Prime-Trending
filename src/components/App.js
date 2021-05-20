d1916e960965c54d7c93a7622af6298828ac9bdd
Jeevanantham <jeevanantham97d@gmail.com>
2021-05-20 21:40:40 +0530

    Added id

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, {Component, useState} from "react";
 import slides from "../data";
 import '../styles/App.css';
-import Slide from "./Slide";
 
 
 const App = () => {
@@ -9,9 +8,12 @@ const App = () => {
   const [Index, setIndex] = useState(() => 0);
 
   return (
-    <div>
-      <Slide props={slides[Index]}/>
-      <div>
+    <div className="App">
+      <div id="slide">
+        <h1 data-testid="title">{slides[Index].title}</h1>
+        <p data-testid="text">{slides[Index].text}</p>
+      </div>
+      <div id="navigation">
         <button data-testid="button-restart" onClick={() => setIndex(0)} disabled={(Index)?false:true}>Restart</button>
         <button data-testid="button-prev" onClick={() => setIndex(prevstate => prevstate - 1)} disabled={(Index)?false:true}>Prev</button>
         <button data-testid="button-next" onClick={() => setIndex(prevstate => prevstate + 1)} disabled={(Index === slides.length-1)?true:false}>Next</button>
