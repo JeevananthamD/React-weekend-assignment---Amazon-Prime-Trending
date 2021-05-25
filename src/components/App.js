ef0c0b684b7a5df084a906ccae91d8397299f328
Jeevanantham <jeevanantham97d@gmail.com>
2021-05-25 16:06:59 +0530

    moved function outside

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,19 @@ import '../styles/App.css';
 
 const App = () => {
 
-  const [Index, setIndex] = useState(() => 0);
+  const [Index, setIndex] = useState(0);
+
+  const restar = () => {
+    setIndex(0);
+  }
+
+  const prev = () => {
+    setIndex(prevstate => prevstate - 1);
+  }
+
+  const next = () => {
+    setIndex(prevstate => prevstate + 1);
+  }
 
   return (
     <div className="App">
@@ -14,9 +26,9 @@ const App = () => {
         <p data-testid="text">{slides[Index].text}</p>
       </div>
       <div id="navigation">
-        <button data-testid="button-restart" onClick={() => setIndex(0)} disabled={(Index)?false:true}>Restart</button>
-        <button data-testid="button-prev" onClick={() => setIndex(prevstate => prevstate - 1)} disabled={(Index)?false:true}>Prev</button>
-        <button data-testid="button-next" onClick={() => setIndex(prevstate => prevstate + 1)} disabled={(Index === slides.length-1)?true:false}>Next</button>
+        <button data-testid="button-restart" onClick={restar} disabled={(Index)?false:true}>Restart</button>
+        <button data-testid="button-prev" onClick={prev} disabled={(Index)?false:true}>Prev</button>
+        <button data-testid="button-next" onClick={next} disabled={(Index === slides.length-1)?true:false}>Next</button>
       </div>
     </div>
   )
